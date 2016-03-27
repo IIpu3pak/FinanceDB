e79dd2950d3b1447e42b9e5d49b87f015d7590f5
Julia Orlova <juliaorlova@gmail.com>
2016-03-27 21:14:07 +0300

    Modify bank class with JPA annotation

--- src/main/java/com/infiniteskills/data/entities/Bank.java
+++ src/main/java/com/infiniteskills/data/entities/Bank.java
@@ -1,29 +1,45 @@
 package com.infiniteskills.data.entities;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "Bank")
 public class Bank {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "BANK_ID")
 	private Long bankId;
 
+	@Column(name = "NAME")
 	private String name;
 
+	@Column(name = "ADDRESS_LINE_1")
 	private String addressLine1;
 
+	@Column(name = "ADDRESS_LINE_2")
 	private String addressLine2;
 
+	@Column(name = "CITY")
 	private String city;
 
+	@Column(name = "STATE")
 	private String state;
 
+	@Column(name = "ZIP_CODE")
 	private String zipCode;
 
+	@Column(name = "LAST_UPDATED_DATE")
 	private Date lastUpdatedDate;
 
+	@Column(name = "LAST_UPDATED_BY")
 	private String lastUpdatedBy;
 
+	@Column(name = "CREATED_DATE")
 	private Date createdDate;
 
+	@Column(name = "CREATED_BY")
 	private String createdBy;
 
 	public Long getBankId() {
